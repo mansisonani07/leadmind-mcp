fe857afd75bd65fbf655cade5f1c3a863098f798
Mansi Sonani <immpatel07@gmail.com>
2026-08-01 16:07:31 +0530

    Change DashboardPage import to named import

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
-import DashboardPage from "@/components/leadmind/DashboardPage";
+import { DashboardPage } from "@/components/leadmind/DashboardPage";
 
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export default function Home() {
   return <DashboardPage />;
